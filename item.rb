7cde8f2a72185f0c55ee2981e2caef18c1d72655
ibugithub <outsideworkibrahim@gmail.com>
2023-08-14 12:07:01 +0600

    fix linters

--- item.rb
+++ item.rb
@@ -1,14 +1,13 @@
 require 'date'
 class Item
-  attr_accessor :archived
   attr_reader :pub_date
 
-  def initialize(id, pub_date, archived: false)
+  def initialize(pub_date, archived: false)
     @id = Random.rand(1..200)
     begin
       @pub_date = Date.strptime(pub_date, '%Y-%m-%d')
     rescue ArgumentError
-      puts "Invalid date format. Please use the format YYYY-MM-DD."
+      puts 'Invalid date format. Please use the format YYYY-MM-DD.'
       exit
     end
     @archived = archived
@@ -16,32 +15,30 @@ class Item
 
   def gener=(gener)
     @gener = gener
+    gener.items.push(self) unless gener.items.include?(self)
   end
 
   def author=(author)
     @author = author
+    author.items.push(self) unless author.items.include?(self)
   end
 
   def source=(source)
     @source = source
+    source.items.push(self) unless source.items.include?(self)
   end
 
   def label=(label)
     @label = label
+    label.items.push(self) unless label.items.include?(self)
   end
 
   def can_be_archived?
     @since_pub_date = years_passed = (Date.today - @pub_date)
-    if years_passed >= 10
-      return true
-    else
-      return false
-    end
+    years_passed >= 10
   end
 
   def move_to_archive
-    if can_be_archived?
-      @archived = true
-    end
+    @archived = true if can_be_archived?
   end
 end
